8014eb0b04b2ef6205360da8154ae25a43e11ad9
Michele Fadda <mikif70@gmail.com>
2015-03-27 15:41:17 +0100

    .

--- redisWrap.go
+++ redisWrap.go
@@ -1,227 +1,228 @@
-// redisWrap
+//// redisWrap
 
 package main
 
-import (
-	"bufio"
-	"errors"
-	"flag"
-	"fmt"
-	"io"
-	"net"
-	"os"
-	"time"
-)
+//import (
+//	"bufio"
+//	"errors"
+//	"flag"
+//	"fmt"
+//	//	"io"
+//	"net"
+//	"os"
+//	"time"
+//)
 
-type Config struct {
-	wrapUrl  string
-	ssdbUrl  string
-	wrapAddr *net.TCPAddr
-	ssdbAddr *net.TCPAddr
-	server   string
-}
+//type Config struct {
+//	wrapUrl  string
+//	ssdbUrl  string
+//	wrapAddr *net.TCPAddr
+//	ssdbAddr *net.TCPAddr
+//	server   string
+//}
 
-type Conn struct {
-	conn     net.Conn
-	bufRead  *bufio.Reader
-	bufWrite *bufio.Writer
-}
+//type Conn struct {
+//	conn     net.Conn
+//	bufRead  *bufio.Reader
+//	bufWrite *bufio.Writer
+//}
 
-var (
-	config = &Config{
-		wrapUrl: "0.0.0.0:6380",
-		ssdbUrl: "10.39.80.181:8888",
-	}
-	okReply   interface{} = "OK"
-	pongReply interface{} = "PONG"
-)
+//var (
+//	config = &Config{
+//		wrapUrl: "0.0.0.0:6380",
+//		ssdbUrl: "10.39.80.181:8888",
+//	}
+//	okReply   interface{} = "OK"
+//	pongReply interface{} = "PONG"
+//)
 
-func (c *Conn) readLine() ([]byte, error) {
-	p, err := c.bufRead.ReadSlice('\n')
-	if err == bufio.ErrBufferFull {
-		return nil, errors.New("long response line")
-	}
-	if err != nil {
-		return nil, err
-	}
-	i := len(p) - 2
-	fmt.Println(string(p), i)
-	if i < 0 || p[i] != '\r' {
-		return nil, errors.New("bad response line terminator")
-	}
-	return p[:i], nil
-}
+//func (c *Conn) readLine() ([]byte, error) {
+//	p, err := c.bufRead.ReadSlice('\n')
+//	if err == bufio.ErrBufferFull {
+//		return nil, errors.New("long response line")
+//	}
+//	if err != nil {
+//		return nil, err
+//	}
+//	i := len(p) - 2
+//	fmt.Println(string(p), i)
+//	if i < 0 || p[i] != '\r' {
+//		return nil, errors.New("bad response line terminator")
+//	}
+//	return p[:i], nil
+//}
 
-func (c *Conn) readReply() (interface{}, error) {
-	line, err := c.readLine()
-	if err != nil {
-		return nil, err
-	}
-	if len(line) == 0 {
-		return nil, errors.New("short response line")
-	}
-	switch line[0] {
-	case '+':
-		switch {
-		case len(line) == 3 && line[1] == 'O' && line[2] == 'K':
-			// Avoid allocation for frequent "+OK" response.
-			return okReply, nil
-		case len(line) == 5 && line[1] == 'P' && line[2] == 'O' && line[3] == 'N' && line[4] == 'G':
-			// Avoid allocation in PING command benchmarks :)
-			return pongReply, nil
-		default:
-			return string(line[1:]), nil
-		}
-	case '-':
-		return fmt.Sprintf("server error: ", string(line[1:])), nil
-	case ':':
-		return parseInt(line[1:])
-	case '$':
-		n, err := parseLen(line[1:])
-		if n < 0 || err != nil {
-			return nil, err
-		}
-		p := make([]byte, n)
-		_, err = io.ReadFull(c.bufRead, p)
-		if err != nil {
-			return nil, err
-		}
-		if line, err := c.readLine(); err != nil {
-			return nil, err
-		} else if len(line) != 0 {
-			return nil, errors.New("bad bulk string format")
-		}
-		return string(p), nil
-	case '*':
-		n, err := parseLen(line[1:])
-		if n < 0 || err != nil {
-			return nil, err
-		}
-		r := make([]interface{}, n)
-		for i := range r {
-			r[i], err = c.readReply()
-			if err != nil {
-				return nil, err
-			}
-		}
-		return r, nil
-	}
-	return nil, errors.New("unexpected response line")
-}
+//func (c *Conn) readReply() (interface{}, error) {
+//	line, err := c.readLine()
+//	if err != nil {
+//		return nil, err
+//	}
+//	if len(line) == 0 {
+//		return nil, errors.New("short response line")
+//	}
+//	switch line[0] {
+//	case '+':
+//		switch {
+//		case len(line) == 3 && line[1] == 'O' && line[2] == 'K':
+//			// Avoid allocation for frequent "+OK" response.
+//			return okReply, nil
+//		case len(line) == 5 && line[1] == 'P' && line[2] == 'O' && line[3] == 'N' && line[4] == 'G':
+//			// Avoid allocation in PING command benchmarks :)
+//			return pongReply, nil
+//		default:
+//			return string(line[1:]), nil
+//		}
+//	case '-':
+//		return fmt.Sprintf("server error: ", string(line[1:])), nil
+//	case ':':
+//		return parseInt(line[1:])
+//	case '$':
+//		n, err := parseLen(line[1:])
+//		if n < 0 || err != nil {
+//			return nil, err
+//		}
+//		//		p := make([]byte, n)
+//		//		_, err = io.ReadFull(c.bufRead, p)
+//		p, err := c.readLine()
+//		if err != nil {
+//			return nil, err
+//		}
+//		//		if line, err := c.readLine(); err != nil {
+//		//			return nil, err
+//		//		} else if len(line) != 0 {
+//		//			return nil, errors.New("bad bulk string format")
+//		//		}
+//		return string(p), nil
+//	case '*':
+//		n, err := parseLen(line[1:])
+//		if n < 0 || err != nil {
+//			return nil, err
+//		}
+//		r := make([]interface{}, n)
+//		for i := range r {
+//			r[i], err = c.readReply()
+//			if err != nil {
+//				return nil, err
+//			}
+//		}
+//		return r, nil
+//	}
+//	return nil, errors.New("unexpected response line")
+//}
 
-// parseInt parses an integer reply.
-func parseInt(p []byte) (interface{}, error) {
-	if len(p) == 0 {
-		return 0, errors.New("malformed integer")
-	}
+//// parseInt parses an integer reply.
+//func parseInt(p []byte) (interface{}, error) {
+//	if len(p) == 0 {
+//		return 0, errors.New("malformed integer")
+//	}
 
-	var negate bool
-	if p[0] == '-' {
-		negate = true
-		p = p[1:]
-		if len(p) == 0 {
-			return 0, errors.New("malformed integer")
-		}
-	}
+//	var negate bool
+//	if p[0] == '-' {
+//		negate = true
+//		p = p[1:]
+//		if len(p) == 0 {
+//			return 0, errors.New("malformed integer")
+//		}
+//	}
 
-	var n int64
-	for _, b := range p {
-		n *= 10
-		if b < '0' || b > '9' {
-			return 0, errors.New("illegal bytes in length")
-		}
-		n += int64(b - '0')
-	}
+//	var n int64
+//	for _, b := range p {
+//		n *= 10
+//		if b < '0' || b > '9' {
+//			return 0, errors.New("illegal bytes in length")
+//		}
+//		n += int64(b - '0')
+//	}
 
-	if negate {
-		n = -n
-	}
-	return n, nil
-}
+//	if negate {
+//		n = -n
+//	}
+//	return n, nil
+//}
 
-// parseLen parses bulk string and array lengths.
-func parseLen(p []byte) (int, error) {
-	if len(p) == 0 {
-		return -1, errors.New("malformed length")
-	}
+//// parseLen parses bulk string and array lengths.
+//func parseLen(p []byte) (int, error) {
+//	if len(p) == 0 {
+//		return -1, errors.New("malformed length")
+//	}
 
-	if p[0] == '-' && len(p) == 2 && p[1] == '1' {
-		// handle $-1 and $-1 null replies.
-		return -1, nil
-	}
+//	if p[0] == '-' && len(p) == 2 && p[1] == '1' {
+//		// handle $-1 and $-1 null replies.
+//		return -1, nil
+//	}
 
-	var n int
-	for _, b := range p {
-		n *= 10
-		if b < '0' || b > '9' {
-			return -1, errors.New("illegal bytes in length")
-		}
-		n += int(b - '0')
-	}
+//	var n int
+//	for _, b := range p {
+//		n *= 10
+//		if b < '0' || b > '9' {
+//			return -1, errors.New("illegal bytes in length")
+//		}
+//		n += int(b - '0')
+//	}
 
-	return n, nil
-}
+//	return n, nil
+//}
 
-func init() {
-	flag.StringVar(&config.ssdbUrl, "s", config.ssdbUrl, "ssdb ip:port")
-	flag.StringVar(&config.wrapUrl, "l", config.wrapUrl, "listen ip:port")
-}
+//func init() {
+//	flag.StringVar(&config.ssdbUrl, "s", config.ssdbUrl, "ssdb ip:port")
+//	flag.StringVar(&config.wrapUrl, "l", config.wrapUrl, "listen ip:port")
+//}
 
-func main() {
-	flag.Parse()
+//func main() {
+//	flag.Parse()
 
-	config.ssdbAddr, _ = net.ResolveTCPAddr("tcp", config.ssdbUrl)
-	config.wrapAddr, _ = net.ResolveTCPAddr("tcp", config.wrapUrl)
+//	config.ssdbAddr, _ = net.ResolveTCPAddr("tcp", config.ssdbUrl)
+//	config.wrapAddr, _ = net.ResolveTCPAddr("tcp", config.wrapUrl)
 
-	fmt.Println(config)
+//	fmt.Println(config)
 
-	fmt.Printf("Listen: %+v\n", config.wrapAddr)
-	ln, err := net.ListenTCP("tcp", config.wrapAddr)
-	if err != nil {
-		fmt.Println("Listen err: ", err.Error())
-		return
-	}
-	defer ln.Close()
+//	fmt.Printf("Listen: %+v\n", config.wrapAddr)
+//	ln, err := net.ListenTCP("tcp", config.wrapAddr)
+//	if err != nil {
+//		fmt.Println("Listen err: ", err.Error())
+//		return
+//	}
+//	defer ln.Close()
 
-	file, err := os.Create("dovecot-redis.log")
-	if err != nil {
-		fmt.Println("File err: ", err.Error())
-		return
-	}
-	defer file.Close()
+//	file, err := os.Create("dovecot-redis.log")
+//	if err != nil {
+//		fmt.Println("File err: ", err.Error())
+//		return
+//	}
+//	defer file.Close()
 
-	ssdb, err := net.DialTCP("tcp", nil, config.ssdbAddr)
-	if err != nil {
-		fmt.Println("Dial err: ", err.Error())
-		return
-	}
-	defer ssdb.Close()
+//	ssdb, err := net.DialTCP("tcp", nil, config.ssdbAddr)
+//	if err != nil {
+//		fmt.Println("Dial err: ", err.Error())
+//		return
+//	}
+//	defer ssdb.Close()
 
-	for {
-		conn, err := ln.AcceptTCP()
-		if err != nil {
-			fmt.Println("Accept err: ", err.Error())
-			continue
-		}
+//	for {
+//		conn, err := ln.AcceptTCP()
+//		if err != nil {
+//			fmt.Println("Accept err: ", err.Error())
+//			continue
+//		}
 
-		go func() {
-			defer conn.Close()
+//		go func() {
+//			defer conn.Close()
 
-			c := &Conn{
-				conn:     conn,
-				bufRead:  bufio.NewReader(conn),
-				bufWrite: bufio.NewWriter(conn),
-			}
+//			c := &Conn{
+//				conn:     conn,
+//				bufRead:  bufio.NewReader(conn),
+//				bufWrite: bufio.NewWriter(conn),
+//			}
 
-			start := time.Now()
-			retval, _ := c.readReply()
-			fmt.Println("parseCmd: ", time.Since(start))
+//			start := time.Now()
+//			retval, _ := c.readReply()
+//			fmt.Println("parseCmd: ", time.Since(start))
 
-			ret := retval.([]interface{})
-			for i := range ret {
-				fmt.Println(ret[i])
-			}
-		}()
+//			ret := retval.([]interface{})
+//			for i := range ret {
+//				fmt.Println(ret[i])
+//			}
+//		}()
 
-	}
-}
+//	}
+//}
--- rlisten.go
+++ rlisten.go
@@ -1,210 +1,65 @@
 // rlisten
 package main
 
-//import (
-//	"bufio"
-//	"bytes"
-//	"flag"
-//	"fmt"
-//	"io"
-//	"net"
-//	"os"
-//	"strconv"
-//	"time"
-//)
+/*
+import (
+	"flag"
+	"fmt"
+	"net"
+)
 
-//type Config struct {
-//	wrapUrl  string
-//	ssdbUrl  string
-//	wrapAddr *net.TCPAddr
-//	ssdbAddr *net.TCPAddr
-//	server   string
-//}
+type Config struct {
+	wrapUrl  string
+	ssdbUrl  string
+	wrapAddr *net.TCPAddr
+	ssdbAddr *net.TCPAddr
+	server   string
+}
 
-//const (
-//	SIMPLE_STRING = '+'
-//	BULK_STRING   = '$'
-//	INTEGER       = ':'
-//	ARRAY         = '*'
-//	ERROR         = '-'
-//)
+const (
+	NL = "\r\n"
+)
 
-//var (
-//	//	ErrInvalidSyntax = error.New("resp: invalid syntax")
-//	config = &Config{
-//		wrapUrl: "0.0.0.0:6380",
-//		ssdbUrl: "10.39.80.181:8888",
-//		server:  "",
-//	}
-//)
+var (
+	//	ErrInvalidSyntax = error.New("resp: invalid syntax")
+	config = &Config{
+		wrapUrl: "0.0.0.0:6380",
+		ssdbUrl: "10.39.80.181:8888",
+		server:  "",
+	}
+)
 
-//type RESPReader struct {
-//	*bufio.Reader
-//}
+func init() {
+	flag.StringVar(&config.ssdbUrl, "s", config.ssdbUrl, "ssdb ip:port")
+	flag.StringVar(&config.wrapUrl, "l", config.wrapUrl, "listen ip:port")
+}
 
-//func NewReader(reader io.Reader) *RESPReader {
-//	return &RESPReader{
-//		Reader: bufio.NewReaderSize(reader, 32*1024),
-//	}
-//}
+func main() {
+	flag.Parse()
 
-//func parseCmd(buf []byte) []byte {
+	config.ssdbAddr, _ = net.ResolveTCPAddr("tcp", config.ssdbUrl)
+	config.wrapAddr, _ = net.ResolveTCPAddr("tcp", config.wrapUrl)
 
-//	row := bytes.Split(buf[:len(buf)-1], []byte("\n"))
-//	ln := len(row)
+	fmt.Println(config)
 
-//	retval := make([][][]byte, 0)
+	wrap, err := net.DialTCP("tcp", nil, config.wrapAddr)
+	if err != nil {
+		fmt.Println("Dial wrap err: ", err.Error())
+		return
+	}
+	defer wrap.Close()
 
-//	for i := 0; i < ln; i++ {
-//		newAr := make([][]byte, 0)
-//		switch row[i][0] {
-//		case '*':
-//			lenAr, _ := strconv.Atoi(string(row[i][1:]))
-//			fmt.Println("Array: ", lenAr)
-//			for a := 1; a <= lenAr*2; a++ {
-//				newAr = append(newAr, row[i])
-//				switch row[i+a][0] {
-//				case '$':
-//					newAr = append(newAr, row[i+a])
-//					fmt.Println("len: ", string(row[i+a][1:]))
-//				default:
-//					if string(row[i+a]) == "MULTI" || string(row[i+a]) == "EXEC" {
-//						newAr = make([][]byte, 0)
-//						break
-//					}
-//					newAr = append(newAr, row[i+a])
-//					fmt.Println("cmd: ", string(row[i+a]))
-//				}
-//			}
-//			retval = append(retval, bytes.Join(newAr, []byte("\n")))
-//			i += lenAr * 2
-//		default:
-//			fmt.Println("not found: ", string(row[i]))
-//		}
-//	}
 
-//	fmt.Println(retval)
+		wrap.Write([]byte("*1" + NL + "$5" + NL + "MULTI" + NL + "*3" + NL + "$6" +
+			NL + "INCRBY" + NL + "$32" + NL + "dtest01@tiscali.it/quota/storage" +
+			NL + "$5" + NL + "-1622" + NL + "*3" + NL + "$6" + NL + "INCRBY" + NL + "$33" +
+			NL + "dtest01@tiscali.it/quota/messages" + NL + "$2" + NL + "-2" + NL + "*1" +
+			NL + "$4" + NL + "EXEC" + NL + "*2" + NL + "$3" + NL + "GET" + NL + "$32" +
+			NL + "dtest01@tiscali.it/quota/storage" + NL))
 
-//	//	ret := bytes.Join(retval, []byte("\n"))
+	buf := make([]byte, 512)
+	n, err := wrap.Read(buf)
+	fmt.Printf("Reply: %+v\n%+v\n", buf[:n], string(buf[:n]))
+}
 
-//	//	fmt.Println(ret)
-
-//	return []byte{}
-//}
-
-//func init() {
-//	flag.StringVar(&config.ssdbUrl, "s", config.ssdbUrl, "ssdb ip:port")
-//	flag.StringVar(&config.wrapUrl, "l", config.wrapUrl, "listen ip:port")
-//	flag.StringVar(&config.server, "c", config.server, "server")
-//}
-
-//func main() {
-//	flag.Parse()
-
-//	config.ssdbAddr, _ = net.ResolveTCPAddr("tcp", config.ssdbUrl)
-//	config.wrapAddr, _ = net.ResolveTCPAddr("tcp", config.wrapUrl)
-
-//	fmt.Println(config)
-
-//	if config.server == "" {
-//		fmt.Printf("Listen: %+v\n", config.wrapAddr)
-//		ln, err := net.ListenTCP("tcp", config.wrapAddr)
-//		if err != nil {
-//			fmt.Println("Listen err: ", err.Error())
-//			return
-//		}
-//		defer ln.Close()
-
-//		file, err := os.Create("dovecot-redis.log")
-//		if err != nil {
-//			fmt.Println("File err: ", err.Error())
-//			return
-//		}
-//		defer file.Close()
-
-//		for {
-//			conn, err := ln.AcceptTCP()
-//			if err != nil {
-//				fmt.Println("Accept err: ", err.Error())
-//				continue
-//			}
-
-//			go func() {
-//				defer conn.Close()
-//				buf := make([]byte, 1024)
-//				nr, err := conn.Read(buf)
-//				if err != nil {
-//					fmt.Println("Read1 err: ", err.Error())
-//					return
-//				}
-
-//				bufRead := buf[:nr]
-
-//				start := time.Now()
-//				bufWrite := parseCmd(bufRead)
-//				fmt.Println("parseCmd: ", time.Since(start))
-
-//				file.Write(bufWrite)
-//				file.Write([]byte("++++++++\n\r"))
-//				fmt.Printf("Read %d bytes:\n%v++++++++\n", nr, string(buf[:nr]))
-
-//				ssdb, err := net.DialTCP("tcp", nil, config.ssdbAddr)
-//				if err != nil {
-//					fmt.Println("Dial err: ", err.Error())
-//					return
-//				}
-//				defer ssdb.Close()
-
-//				ssdb.Write(buf[:nr])
-
-//				nr, err = ssdb.Read(buf)
-//				if err != nil {
-//					fmt.Println("Read2 err: ", err.Error())
-//					conn.Write([]byte("+OK\r\n"))
-//					return
-//				}
-
-//				conn.Write(buf[:nr])
-//				fmt.Printf("Reply %d bytes:\n%v--------\n\n", nr, string(buf[:nr]))
-//				file.Write(buf[:nr])
-//				file.Write([]byte("--------\n\r"))
-
-//			}()
-//		}
-//	}
-
-//	wrap, err := net.DialTCP("tcp", nil, config.wrapAddr)
-//	if err != nil {
-//		fmt.Println("Dial wrap err: ", err.Error())
-//		return
-//	}
-//	defer wrap.Close()
-
-//	wrap.Write([]byte(`*1
-//$5
-//MULTI
-//*3
-//$6
-//INCRBY
-//$32
-//dtest01@tiscali.it/quota/storage
-//$5
-//-1622
-//*3
-//$6
-//INCRBY
-//$33
-//dtest01@tiscali.it/quota/messages
-//$2
-//-2
-//*1
-//$4
-//EXEC
-//*2
-//$3
-//GET
-//$32
-//dtest01@tiscali.it/quota/storage
-//`))
-
-//}
+*/
